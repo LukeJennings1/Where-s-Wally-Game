2013fb9a3210db9a1599dec8aac33413c9a7886f
Luke Jennings <lukejennings123@gmail.com>
2022-10-21 14:33:29 +0100

    added conditional modal render

--- src/content.js
+++ src/content.js
@@ -1,11 +1,28 @@
 import wallyLevel1 from './assets/wally-level-1.png'
+import Dropdown from './dropdown';
+import { useState } from 'react'
 
 function Content() {
-    return (
-        <div className="content-wrapper">
-            <img id = 'wally-level-1-image' src={wallyLevel1}></img>
-        </div>
+
+    const [dropDown, setDropDown] = useState(false)
+
+const adddropdown = () => {
+    return ( 
+        <select>
+            <option value={'Wally'}>Wally</option>
+            <option value={'Wizard'}>Wizard</option>
+            <option value={'Odlaw'}>Odlaw</option>
+        </select>
     )
 }
 
+    return (
+        <div className="content-wrapper">
+            {dropDown && adddropdown()} 
+            {/* ^^ if the state and dropdown are equal to true render dropdown. As the Dropdown is always true 
+            we just need to adjust the dropDown to true on a click like the one in the img below. */}
+            <img onClick={() => {setDropDown(true)}} id = 'wally-level-1-image' src={wallyLevel1}></img>
+        </div>
+    )
+}   
 export default Content;
---
+++ src/dropdown.js
@@ -0,0 +1,9 @@
+function Dropdown() {
+    return (
+        <select>
+            <option value={'wally'}>Wally</option>
+        </select>
+    )
+}
+
+export default Dropdown;
